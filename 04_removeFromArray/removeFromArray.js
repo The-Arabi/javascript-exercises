6a331b4301da99eb188381ffe49a3411b8b2a370
The-Arabi <100914809+The-Arabi@users.noreply.github.com>
2023-06-08 17:22:52 +0300

    Finish excersise 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+//1st solution
+/*const removeFromArray = function(array, ...value) {
+    let final_array = [];
+    for (let item of array) {
+            if (value.indexOf(item) == -1) {
+                final_array.push(item);
+            }
+    }
+    return final_array;
+};*/ 
 
+// 2nd solution
+const removeFromArray = function(array, ...value) {
+    for (item of value) {
+        if (array.indexOf(item) != -1) {
+            array.splice(array.indexOf(item),1)
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
